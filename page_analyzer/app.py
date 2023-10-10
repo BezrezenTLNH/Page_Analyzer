3cea53369c4a74a5c5cfb15dea3e944061042dc9
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-10-10 19:47:43 +0200

    5 part of first step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,12 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
 
 
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+
+@app.get('/')
+def root_get():
+    return 'Hello!'
