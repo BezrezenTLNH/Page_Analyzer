2f57f94154bb0c252087a2c64ab3dde475c08b30
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-10-17 15:22:13 +0200

    last part of step 3

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,45 +16,41 @@ def root_get():
 
 
 @app.route('/urls')
-def urls():
-    urls = db.get_all_data()
+def urls_get():
+    urls = db.get_data()
     return render_template('urls.html', urls=urls)
 
 
 @app.route('/urls', methods=['POST'])
-def create_urls():
+def urls_post():
     url = request.form.get('url')
 
     if not url:
         flash('URL обязателен', 'danger')
-        messages = get_flashed_messages(with_categories=True)
-        return render_template('main.html', messages=messages), 422
-
-    if not db.url_valide(url):
-        flash('Некорректный URL', 'danger')
-        messages = get_flashed_messages(with_categories=True)
-        return render_template('main.html', messages=messages), 422
+        msgs = get_flashed_messages(with_categories=True)
+        return render_template('main.html', msgs=msgs), 422
 
     url = db.url_normalize(url)
 
-    if db.get_id(url):
-        id = db.get_id(url)
-        flash('Страница уже существует', 'alert-info')
-        return redirect(url_for('url_get', id=id))
+    if not db.url_validate(url):
+        flash('Некорректный URL', 'danger')
+        msgs = get_flashed_messages(with_categories=True)
+        return render_template('main.html', msgs=msgs), 422
 
     id = db.add_data(url)
 
-    if id is None:
-        flash('Something wrong', 'alert-danger')
-        msgs = get_flashed_messages(with_categories=True)
-        return render_template('index.html', url=url, msgs=msgs), 422
+    if db.get_id(url):
+        id = db.get_id(url)
+        flash('Страница уже существует', 'info')
+        return redirect(url_for('url_get', id=id), code=302)
 
-    flash('Страница успешно добавлена', 'alert-success')
-    return redirect(url_for('get_site', id=id), code=302)
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('url_get', id=id), code=302)
 
 
-@app.route('/urls/<int:id>', methods=['GET'])
-def get_url(id):
-    url = db.get_url_data(id)
+@app.route('/urls/<int:id>')
+def url_get(id):
     msgs = get_flashed_messages(with_categories=True)
-    return render_template('url_get.html', url=url, msgs=msgs)
+    url = db.get_url_data(id)
+
+    return render_template('url.html', url=url, msgs=msgs), 422
--- page_analyzer/db_commands.py
+++ page_analyzer/db_commands.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
-from datetime import datetime
+import psycopg2.extras
+from datetime import date
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 from validators import url as valid
@@ -11,7 +12,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 def url_normalize(url):
-    url = url = urlparse(url)
+    url = urlparse(url)
     return f'{url.scheme}://{url.netloc}'
 
 def url_validate(url):
@@ -20,67 +21,46 @@ def url_validate(url):
 
 
 def get_id(url):
-    connection = psycopg2.connect(DATABASE_URL)
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT id FROM urls WHERE name=%s", (url,))
-        id = cursor.fetchone()
-        connection.close()
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(
+                cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute("SELECT id FROM urls WHERE name=%s", (url,))
+            id = cursor.fetchone()['id']
 
-    return id if id else None
+            return id if id else None
+
+
+def get_url_data(id):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(
+                cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute("SELECT * FROM urls WHERE id=%s", (id,))
+
+            return cursor.fetchone()
 
 
 def add_data(url):
     if get_id(url):
         return None
-    else:
-        try:
-            with psycopg2.connect(DATABASE_URL) as conn:
-                with conn.cursor() as cur:
-                    cur.execute('INSERT INTO urls (name, created_at)'
-                                'VALUES (%s, %s) RETURNING id',
-                                (url, datetime.now())
-                                )
-                    conn.commit()
 
-                    id = cur.fetchone()
-
-                    return id
-
-        except psycopg2.Error:
-            return None
-
-        finally:
-            conn.close()
-
-
-def get_all_data():
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                "SELECT * FROM urls"
-            )
-            rows = cur.fetchone()
+        with conn.cursor(
+                cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute('INSERT INTO urls (name, created_at)'
+                        'VALUES (%s, %s)',
+                        (url, date.today().isoformat())
+                        )
+            conn.commit()
 
-    urls = ({'id': row.id,
-             'name': row.name,
-             'created_at': row.created_at} for row in rows)
+            id = get_id(url)
 
-    return urls
-
-
+            return id
 
 
-def get_url_data(id):
-        conn = psycopg2.connect(DATABASE_URL)
+def get_data():
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(
+                cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute('SELECT * FROM urls ORDER BY id DESC')
 
-        with conn.cursor() as cur:
-            cur.execute(
-                "SELECT * FROM urls WHERE id=%s",
-                (id)
-            )
-            row = cur.fetchone()
-
-        return {'id': row.id,
-                'name': row.name,
-                'created_at': row.created_at
-                }
+            return cur.fetchall()
