210ce4da312d0fe72a98967da2ff692eade5f80b
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-11-01 12:57:12 +0100

    change tests and hide some files

--- page_analyzer/tests/test_app.py
+++ page_analyzer/tests/test_app.py
@@ -1,17 +1,15 @@
 import os
 
-# import psycopg2
-# import psycopg2.extras
+import page_analyzer.db_commands as pa
 import pytest
 from dotenv import load_dotenv
-from playwright.sync_api import Page, expect
-
 import page_analyzer
 
 
 load_dotenv()
-URL = 'https://page-analyzer-zx8t.onrender.com/'
+URL = 'http://localhost:8000'
 DATABASE_URL = os.getenv('DATABASE_URL')
+TESTING_URL = 'https://career.habr.com'
 
 
 @pytest.fixture()
@@ -34,64 +32,18 @@ def test_get_root(client):
     assert response.status_code == 200
 
 
-# def test_check_url(page: Page):
-#     page.goto(f'{URL}/', timeout=0)
-#     page.get_by_placeholder("https://www.example.com"). \
-#         fill("")
-#     page.locator('input[type="submit"]').click()
-#     expect(page.get_by_text("URL обязателен")).to_be_visible()
-#
-#     page.get_by_placeholder("https://www.example.com"). \
-#         fill("test@")
-#     page.locator('input[type="submit"]').click()
-#     expect(page.get_by_text("Некорректный URL")).to_be_visible()
-#
-#     page.goto(f'{URL}/', timeout=0)
-#     page.get_by_placeholder("https://www.example.com"). \
-#         fill("https://test.com")
-#     page.locator('input[type="submit"]').click()
-#     expect(page.get_by_text("Страница успешно добавлена")).to_be_visible()
-#
-#     page.goto(f'{URL}/', timeout=0)
-#     page.get_by_placeholder("https://www.example.com"). \
-#         fill("https://TEST.com")
-#     page.locator('input[type="submit"]').click()
-#     expect(page.get_by_text("Страница уже существует")).to_be_visible()
-#
-#     with psycopg2.connect(DATABASE_URL) as conn:
-#         with conn.cursor(
-#                 cursor_factory=psycopg2.extras.DictCursor) as cursor:
-#             cursor.execute('DELETE FROM urls WHERE name = %s',
-#                            ('https://test.com',))
-#             conn.commit()
+def test_add_data():
+    pa.add_data(TESTING_URL)
+    id = pa.get_id(TESTING_URL)
+    assert pa.get_url_data(id)['name'] == 'https://career.habr.com'
 
 
-def test_urls_get(page: Page):
-    page.goto(f'{URL}/urls', timeout=0)
-    expect(page.get_by_role("heading", name="Сайты")).to_be_visible()
-    expect(page.get_by_role("table", name='')).to_be_visible()
-
-
-# def test_run_check(page: Page):
-#     page.goto(f'{URL}/', timeout=0)
-#     page.get_by_placeholder("https://www.example.com"). \
-#         fill("https://aaa.ru")
-#     page.locator('input[type="submit"]').click()
-#     page.locator('text=Запустить проверку').click()
-#     expect(page.get_by_text("Страница успешно проверена")).to_be_visible()
-#
-#     page.goto(f'{URL}/', timeout=0)
-#     page.get_by_placeholder("https://www.example.com"). \
-#         fill("https://ccc.com")
-#     page.locator('input[type="submit"]').click()
-#     page.locator('text=Запустить проверку').click()
-#     expect(page.get_by_text("Произошла ошибка при проверке")).to_be_visible()
-#
-#     with psycopg2.connect(DATABASE_URL) as conn:
-#         with conn.cursor(
-#                 cursor_factory=psycopg2.extras.DictCursor) as cursor:
-#             cursor.execute('DELETE FROM urls WHERE name = %s',
-#                            ('https://aaa.ru',))
-#             cursor.execute('DELETE FROM urls WHERE name = %s',
-#                            ('https://ccc.com',))
-#             conn.commit()
+def test_check_url():
+    id = pa.get_id(TESTING_URL)
+    status_code, h1, title, description = pa.parser(TESTING_URL)
+    pa.check_url(id, status_code, h1, title, description)
+    assert pa.get_check_url(id)[0]['id'] == 1
+    assert pa.get_check_url(id)[0]['status_code'] == 200
+    assert pa.get_check_url(id)[0]['title'] == \
+           'Работа в IT-индустрии, свежие вакансии и резюме,' \
+           ' поиск работы — Хабр Карьера'
